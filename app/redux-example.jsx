318d60e3d2371bd46ee3dcd88ba257f3afb2bd58
Adam <AdamBCo@me.com>
2016-06-12 11:47:41 -0700

    Getting the hang of subscribers

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,6 +4,19 @@ console.log('Starting Redux Example');
 
 var reducer = (state, action) => {
   state = state || {name: "Anonymous"};
+
+  console.log('New Action', action);
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+      break;
+    default:
+      return state;
+  }
+
   return state;
 }
 
@@ -11,3 +24,10 @@ var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Adam'
+};
+
+store.dispatch(action);
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -9,10 +9,40 @@ var stateDefault = {
 };
 
 var reducer = (state = stateDefault, action) => {
+
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+      break;
+    default:
+      return state;
+  }
+
   return state;
 }
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+//Subscribe
+
+store.subscribe(() => {
+  var state = store.getState();
+  document.getElementById('app').innerHTML = state.searchText;
+});
 
 var currentState = store.getState();
-console.log('currentState', currentState);
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'HELLO'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'COOL'
+});
